dad176aa8b0ec64c48121ca3a84220a14c4fa25b
qi-ou <ouqi1005@gmail.com>
2022-11-15 14:19:03 +0000

    try iterative correction

--- bin/LiCSBAS132_3D_correction.py
+++ bin/LiCSBAS132_3D_correction.py
@@ -186,7 +186,6 @@ def get_para():
     else:
         raise Exception("No input threshold or info/131resid_2pi*.txt file, quit...")
 
-
     print("Correction threshold = {:2f}".format(correction_thresh))
     print("Target threshold = {:2f}".format(target_thresh))
 
@@ -203,9 +202,9 @@ def correction_decision(ifg_list=None):
     global bad_ifg_not_corrected, ifg_corrected_by_mode, ifg_corrected_by_integer, good_ifg
 
     # set up empty ifg lists
-    good_ifg = []
-    ifg_corrected_by_mode = []
-    ifg_corrected_by_integer = []
+    # good_ifg = []
+    # ifg_corrected_by_mode = []
+    # ifg_corrected_by_integer = []
     bad_ifg_not_corrected = []
 
     # automatic correction
@@ -297,7 +296,7 @@ def correction_decision(ifg_list=None):
                 # if component mode is useful
                 if rms_res_mode_corrected < target_thresh:
                     print(
-                        "Component modes reduces rms residuals to {:.2f}, below threshold of {:.2f}, correcting by component mode...".format(
+                        "Component modes reduces rms residuals to {:.2f}, below target threshold of {:.2f}, correcting by component mode...".format(
                             rms_res_mode_corrected, target_thresh))
                     unw_corrected = unw - res_mode * 2 * np.pi
                     correction_title = "Mode_corrected"
@@ -411,11 +410,17 @@ def plot_networks():
 
 
 def main():
-    global correction_thresh, target_thresh
+    global correction_thresh, target_thresh, good_ifg, ifg_corrected_by_mode, ifg_corrected_by_integer
     start()
     init_args()
     set_input_output()
     get_para()
+
+    # set up
+    good_ifg = []
+    ifg_corrected_by_mode = []
+    ifg_corrected_by_integer = []
+
     # correction_decision()
     # save_lists()
     # n_gap = plot_networks()
@@ -424,8 +429,7 @@ def main():
         print("n_gap=" + str(n_gap)+", increase correction_thresh and target_thresh by 0.1")
         correction_thresh += 0.1
         target_thresh += 0.1
-        bad_ifg = io_lib.read_ifg_list('./TS_GEOCml10GACOS/info/132bad_ifg1_0.20_0.20.txt')
-        correction_decision(bad_ifg)
+        correction_decision(bad_ifg_not_corrected)
         save_lists()
         n_gap = plot_networks()
     finish()
